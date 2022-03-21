8d09161535bce94f12a9b0c3acf7871ab5d636e3
José Fernandes <zefrancisco2002@gmail.com>
2022-03-21 11:48:28 +0000

    Some done exercises

---
+++ Programas/otimizacao-1a.s
@@ -0,0 +1,39 @@
+		.data
+i:		.word	0
+C:		.word	7
+A:		.space	800
+B:		.space	800
+
+		.text
+
+	DADDI	R1,R0,0	;inicializa i 
+	SD	R1, i(R0)	;armazena i 
+	
+loop:
+
+	LD	R1,i(R0)	;obtem o valor de i 
+	DADDI	R9,R0,8		;tamanho da dword para multiplicação pelo indice
+	DMUL	R2,R1,R9	;R2 = offset de i
+;6 bolhas
+	LD	R4,B(R2)	;carrega o valor de B[i] 
+	LD	R5,C(R0)	;carrega o valor de C 
+
+	DADD	R6,R4,R5	;B[i]+C 
+;1 bolha
+	LD	R1,i(R0)	;obtem o valor de i 
+	DADDI	R9,R0,8		;tamanho da dword para multiplicação pelo indice
+	DMUL	R2,R1,R9	;R2 = offset de i
+;6 bolhas
+	SD	r6,A(r2)	;A[i] = B[i]+C 
+
+	LD	R1,i(R0)	;obtem o valor de i 
+;1 bolha
+	DADDI	R1,R1,1		;incrementa i 
+	SD	r1,i(R0)	;armazena i 
+
+	LD	R1,i(R0)		;obtem o valor de i
+;1 bolha
+	DADDI	R8,R1,-100	;o contador chegou a 100? 
+	BNEZ	R8,loop		;se não chegou, repete o ciclo 
+;1 bolha de controlo
+	halt			;termina a execução 
---
+++ Programas/otimizacao-1d.s
@@ -0,0 +1,28 @@
+		.data
+i:		.word	0
+C:		.word	7
+A:		.space	800
+B:		.space	800
+
+		.text
+;i tem R1 atribuido
+;valor 8 vai ficar reservado para o registo r9
+;i = 0
+	DADDI	R9,R0,8		;tamanho da dword para multiplicação pelo indice <------
+	LD	R5,C(R0)	;carrega o valor de C <---
+	
+loop:
+	LD	R4,B(R2)	;carrega o valor de B[i] <---
+;1 bolha
+	DADDI R8,R1, -792; <--- 800-8
+	DADD	R6,R4,R5	;B[i]+C 
+	SD	r6,A(r2)	;A[i] = B[i]+C <---
+	
+	DADDI 	R1,R1,8 ; <---
+;	DADDI	R8,R1, -800
+;1 bolha
+	BNEZ	R8,loop		;se não chegou, repete o ciclo
+	DSRA R1,R1,3; r1/8
+	SD r1, i(R0); 
+;1 bolha de controlo
+	halt			;termina a execução 
